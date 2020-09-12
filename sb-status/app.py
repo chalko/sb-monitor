f52249f926c2aa68b6f99a17d4fb4bacf9483983
Nick Chalko <nick@chalko.com>
2020-09-11 21:33:31 -0700

    Fetch the data, and cache it.

--- sb-status/app.py
+++ sb-status/app.py
@@ -1,8 +1,15 @@
 # flask_web/app.py
 
 from flask import Flask
+import requests
+import json
+import requests_cache
+
+requests_cache.install_cache()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    r=requests.get("http://192.168.100.1")
+    return r.text
